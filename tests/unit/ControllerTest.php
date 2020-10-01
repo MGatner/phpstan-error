1be5840140af28023be578d040fedfab6e1a0446
MGatner <mgatner@icloud.com>
2020-10-01 15:05:57 +0000

    Initial upload

---
+++ tests/unit/ControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use CodeIgniter\Test\CIUnitTestCase;
+
+class ControllerTest extends CIUnitTestCase
+{
+	use \CodeIgniter\Test\ControllerTester;
+
+	public function testError()
+	{
+		$this->assertTrue(true);
+	}
+}
